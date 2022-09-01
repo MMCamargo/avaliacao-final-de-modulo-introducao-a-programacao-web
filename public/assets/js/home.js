863a7074d2c30f26e603f5c971ecc88df42024ed
MMCamargo <matheusmarianodecamargo@gmail.com>
2022-09-01 01:00:29 -0300

    Refatora função de edição de recados

--- public/assets/js/home.js
+++ public/assets/js/home.js
@@ -2,31 +2,19 @@
 
 const users = getItem('users')
 const loggedUser = getItem('loggedUser')
-const loggedUserData = users.find((value) => value.email === loggedUser.email)
-
-document.addEventListener('DOMContentLoaded', () => {
-    loggedUserData.notes.forEach((value) => loadNotes(value))
-})
 
 const notesModal = document.getElementById('notes-modal')
-const notesModalForm = document.getElementById('notes-modal__form')
 const notesModalTitle = document.getElementById('notes-modal__title-input')
 const notesModalDescription = document.getElementById('notes-modal__description-input')
 const notesModalBtn = document.getElementById('notes-modal__submit-input')
 
-const table = document.getElementById('table')
+const table = document.getElementById('table__body')
+
+loadNotes()
 
 window.addEventListener('click', (event) => event.target === notesModal ? resetNotesModal() : null)
 
 document.addEventListener('keydown', (event) => event.key === 'Escape' ? resetNotesModal() : null)
-
-notesModalBtn.addEventListener('click', (event) => {
-    event.preventDefault()
-
-    notesModal.style.display = 'none'
-
-    addNotes()
-})
 
 const logoutBtn = document.getElementById('header__logout-btn')
 const addBtn = document.getElementById('add-btn')
@@ -52,10 +40,13 @@ function updateUserData(updatedData) {
 function resetNotesModal() {
     notesModal.style.display = 'none'
 
-    notesModalForm.reset()
+    notesModalTitle.value = ''
+    notesModalDescription.value = ''
 }
 
 function addNotes() {
+    let loggedUserData = users.find((value) => value.email === loggedUser.email)
+
     const note = {
         title: notesModalTitle.value,
         description: notesModalDescription.value,
@@ -64,89 +55,93 @@ function addNotes() {
 
     loggedUserData.notes.push(note)
 
-    notesModalForm.reset()
+    notesModalTitle.value = ''
+    notesModalDescription.value = ''
 
     updateUserData(loggedUserData)
 
-    loadNotes(note)
+    notesModal.style.display = 'none'
+
+    loadNotes()
 }
 
-function loadNotes(value) {
-    const row = document.createElement('tr')
-    row.classList.add('table__body-row')
-    row.setAttribute('id', value.id)
+function loadNotes() {
+    let loggedUserData = users.find((value) => value.email === loggedUser.email)
 
-    const title = document.createElement('td')
-    title.classList.add('table__body-cell')
-    title.innerText = value.title
+    table.innerHTML = ''
 
-    const description = document.createElement('td')
-    description.classList.add('table__body-cell')
-    description.innerText = value.description
+    for (const value of loggedUserData.notes) {
+        const row = document.createElement('tr')
+        row.classList.add('table__body-row')
+        row.setAttribute('id', value.id)
 
-    const btnCell = document.createElement('td')
-    btnCell.classList.add('table__body-cell')
+        const title = document.createElement('td')
+        title.classList.add('table__body-cell')
+        title.innerText = value.title
 
-    const editBtn = document.createElement('span')
-    editBtn.classList.add('material-symbols-rounded')
-    editBtn.innerText = 'edit_note'
-    editBtn.addEventListener('click', () => editNotes(value.id))
+        const description = document.createElement('td')
+        description.classList.add('table__body-cell')
+        description.innerText = value.description
 
-    const deleteBtn = document.createElement('span')
-    deleteBtn.classList.add('material-symbols-rounded')
-    deleteBtn.innerText = 'delete'
-    deleteBtn.addEventListener('click', () => deleteNotes(value.id))
+        const btnCell = document.createElement('td')
+        btnCell.classList.add('table__body-cell')
 
-    btnCell.appendChild(editBtn)
-    btnCell.appendChild(deleteBtn)
+        const editBtn = document.createElement('span')
+        editBtn.classList.add('material-symbols-rounded')
+        editBtn.innerText = 'edit_note'
+        editBtn.addEventListener('click', () => editNotes(value.id))
 
-    row.appendChild(title)
-    row.appendChild(description)
-    row.appendChild(btnCell)
+        const deleteBtn = document.createElement('span')
+        deleteBtn.classList.add('material-symbols-rounded')
+        deleteBtn.innerText = 'delete'
+        deleteBtn.addEventListener('click', () => deleteNotes(value.id))
 
-    table.appendChild(row)
+        btnCell.appendChild(editBtn)
+        btnCell.appendChild(deleteBtn)
+
+        row.appendChild(title)
+        row.appendChild(description)
+        row.appendChild(btnCell)
+
+        table.appendChild(row)
+    }
 }
-
 
 function editNotes(id) {
-    const editedNote = loggedUserData.notes.find((value) => value.id === id)
-    const editedRow = document.getElementById(id)
+    let loggedUserData = users.find((value) => value.email === loggedUser.email)
+
+    const editedNote = loggedUserData.notes.findIndex((value) => value.id === id)
 
     notesModal.style.display = 'grid'
 
-    notesModalTitle.value = editedNote.title
-    notesModalDescription.value = editedNote.description
+    notesModalTitle.value = loggedUserData.notes[editedNote].title
+    notesModalDescription.value = loggedUserData.notes[editedNote].description
 
-    notesModalBtn.addEventListener('click', (event) => {
-        event.preventDefault()
+    notesModalBtn.innerText = 'Atualizar'
 
-        editedRow.remove()
-
-        updateNotes(id)
-    })
+    notesModalBtn.onclick = () => updateNotes(editedNote, loggedUserData)
 }
 
-function updateNotes(id) {
-    const editedNoteIndex = loggedUserData.notes.findIndex((value) => value.id === id)
-    const editedTitle = notesModalTitle.value
-    const editedDescription = notesModalTitle.value
-
-    const note = {
-        title: editedTitle,
-        description: editedDescription,
-        id: id
-    }
-
-    loggedUserData.notes.forEach((value, index) => {
-        if (value.id === id) {
-            index = note
-        }
-    })
+function updateNotes(editedNote, loggedUserData) {
+    loggedUserData.notes[editedNote].title = notesModalTitle.value
+    loggedUserData.notes[editedNote].description = notesModalDescription.value
 
     updateUserData(loggedUserData)
+
+    loadNotes()
+
+    notesModal.style.display = 'none'
+
+    setTimeout(() => {
+        notesModalBtn.innerText = 'Salvar'
+
+        notesModalBtn.onclick = () => addNotes()
+    }, 1000)
 }
 
 function deleteNotes(id) {
+    let loggedUserData = users.find((value) => value.email === loggedUser.email)
+
     const deletedNoteIndex = loggedUserData.notes.findIndex((value) => value.id === id)
 
     const deletedRow = document.getElementById(id)
